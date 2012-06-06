dd4e785b820d7acf03a65fb5194ca8ad847318cc
Amanda Bergeron <bergeron@goldmail.etsu.edu>
2012-06-06 12:39:21 -0400

    G1 capturing an order and made a slight ajax change

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,9 +1,9 @@
 class LineItem < ActiveRecord::Base
-
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
 
-  attr_accessible :cart_id, :product_id
+  attr_accessible :cart_id, :product_id, :order_id
 
   def total_price
      product.price * quantity
